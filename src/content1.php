c323530bbf38f7953e5b8b70af35893c8647d3f4
jaksas <jaksas@onid.oregonstate.edu>
2015-02-05 02:26:40 -0600

    A bunch of wasted time trying to make something impossible happen witht the logout

--- src/content1.php
+++ src/content1.php
@@ -1,18 +1,39 @@
 <?php
+
 $filePath = explode('/', $_SERVER['PHP_SELF'], -1);
 $filePath = implode('/', $filePath); 
 $login = "http://".$_SERVER['HTTP_HOST'].$filePath."/login.php"; 
 $content2 = "http://".$_SERVER['HTTP_HOST'].$filePath."/content2.php"; 
 
+session_start();
+
+function foo() {
+	echo "foo";
+}
+
 if(!isset ($_POST['username']) && $_SESSION['username'] == null) {
 	header("Location: {$login}");
 }
 
-if($_POST['username'] === '') {
+if(isset ($_POST['username']) && $_POST['username'] === '') {
 	echo 'A username must be entered. Click ';
 	echo "<a href={$login}>here</a>";
+	echo " to return to the login screen."; 
 }
 
-//else(
+else {
+	if(isset($_POST['username'])) { 
+		$_SESSION['username'] = $_POST['username'];
+		$_SESSION['num_visits'] = 1;
+	}
 
+	
+	else {
+		$_SESSION['num_visits'] += 1;
+	}
+	
+	echo "Hello {$_SESSION['username']} you have visited this page 
+		{$_SESSION['num_visits']} times before. Click ";
+	echo "<a href={$login}>here</a> to logout."; 
+}
 ?>
