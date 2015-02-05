7db03142faa7240275a796c46a53dbb4de7b68d4
jaksas <jaksas@onid.oregonstate.edu>
2015-02-04 23:44:56 -0600

    Begin session, added ability check for prior login

--- src/content1.php
+++ src/content1.php
@@ -4,8 +4,15 @@ $filePath = implode('/', $filePath);
 $login = "http://".$_SERVER['HTTP_HOST'].$filePath."/login.php"; 
 $content2 = "http://".$_SERVER['HTTP_HOST'].$filePath."/content2.php"; 
 
+if(!isset ($_POST['username']) && $_SESSION['username'] == null) {
+	header("Location: {$login}");
+}
+
 if($_POST['username'] === '') {
 	echo 'A username must be entered. Click ';
 	echo "<a href={$login}>here</a>";
 }
+
+//else(
+
 ?>
